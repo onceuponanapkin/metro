50e7af1bafe8631cff4eea04ec5f0726e093484f
onceuponanapkin <suedes@gmail.com>
2015-04-16 09:21:24 -0400

    Update order.php

--- order.php
+++ order.php
@@ -1,42 +1,88 @@
 <?php
+include 'config.php';
 
-include 'config.php'
+error_reporting (E_ALL ^ E_NOTICE);
 
-if(empty($_POST['unleaded'])   ||    
-   empty($_POST['mid'])  ||
-   empty($_POST['premium'])||   
-   empty($_POST['hwy'])  ||
-   empty($_POST['dyed'])||  
-   empty($_POST['rec'])  ||
-   empty($_POST['kerosen'])||  
-  {     
-       echo "No arguments Provided!";   return false;    
-  } 
+$post = (!empty($_POST)) ? true : false;
 
-$customer = ($_post['customer']);
-$phone = ($_post['phone#']);
-$unleaded = ($_POST['unleaded']);
-$mid = ($_POST['mid']);
-$premium = ($_POST['premium']);
-$hwy = ($_POST['hwy']);
-$dyed = ($_POST['dyed']);
-$rec = ($_POST['rec']);
-$ker = ($_POST['kerosen']);
-$message = ($_POST['message']);
+if($post)
+{
 
-// create email body and send it    
- $mail =  mail(WEBMASTER_EMAIL, $email_subject, $email_body, 
- // put your email 
- $email_subject = "Website order submitted by:  $customer#";
- $email_body = "You have received a new order. \n\n".                 
-                   " Here are the details:\n \nName: $customer# \n ".                  
-                   "Fuel Order \n $unleaded, $mid, $premium, $hwy, $dyed, $rec, $ker Message \n $message"
-                   ; 
- $headers = "From: orders@metropetro.net\n"; 
- $headers .= "Reply-To: $phone#";     
- 
- mail($to,$email_subject,$email_body,$headers); return true;             
- 
-// send the user back to the form
-header('Location: /#='.urlencode('Order Submitted')); exit;
+$customer = Trim(stripslashes($_POST['customer']));
+$email = Trim(stripslashes($_POST['email']));
+$tel = Trim(stripslashes($_POST['tel'])); 
+$subject = "You have a new web order:  $customer";
+$unleaded = Trim(stripslashes($_POST['unleaded']));
+$mid = Trim(stripslashes($_POST['mid'])); 
+$premium = Trim(stripslashes($_POST['premium']));
+$hwy = Trim(stripslashes($_POST['hwy']));
+$dyed = Trim(stripslashes($_POST['dyed']));
+$rec = Trim(stripslashes($_POST['rec']));  
+$kerosene = Trim(stripslashes($_POST['kerosene']));
+$date = Trim(stripslashes($_POST['date']));  
+$notes = Trim(stripslashes($_POST['notes'])); 
+
+
+// prepare email body text
+$body = "";
+$body .= "Customer#: ";
+$body .= $customer;
+$body .= "\n";
+$body .= "Email: ";
+$body .= $email;
+$body .= "\n";
+$body .= "Phone#: ";
+$body .= $tel;
+$body .= "\n";
+$body .= "Delivery Date: ";
+$body .= $date;
+$body .= "\n";
+$body .= "Unleaded: ";
+$body .= $unleaded;
+$body .= "\n";
+$body .= "Midgrade: ";
+$body .= $mid;
+$body .= "\n";
+$body .= "Premium: ";
+$body .= $premium;
+$body .= "\n";
+$body .= "Hwy Diesel: ";
+$body .= $hwy;
+$body .= "\n";
+$body .= "Dyed Diesel: ";
+$body .= $dyed;
+$body .= "\n";
+$body .= "Rec 90: ";
+$body .= $rec;
+$body .= "\n";
+$body .= "Kerosene: ";
+$body .= $kerosene;
+$body .= "\n";
+$body .= "Special Instructions: ";
+$body .= $notes;
+$body .= "\n";
+
+$error = '';
+
+if(!$error)
+{
+$mail = mail(WEBMASTER_EMAIL, $subject, $body,
+     "From: ".$customer." <".$email.">\r\n"
+    ."Reply-To: ".$email."\r\n"
+    ."X-Mailer: PHP/" . phpversion());
+
+
+if($mail)
+{
+$sendmessage = "Sent! Thank you.";
+}
+else {
+       $message = "Oops...";
+   }
+   echo '<div id="message">'.$message.'<div id="close-button"></div></div>';
+}
+
+
+}
+
 ?>
